1db644322cd28cbd640f03f8b40fa18229576697
GedMarc <ged_marc@hotmail.com>
2018-05-24 21:06:17 +0200

    Persistence Loader

--- src/main/java/za/co/mmagon/guiceinjection/db/GuicedPersistenceBinding.java
+++ src/main/java/za/co/mmagon/guiceinjection/db/GuicedPersistenceBinding.java
@@ -1,13 +1,17 @@
 package za.co.mmagon.guiceinjection.db;
 
 import com.google.inject.Key;
+import com.google.inject.Singleton;
 import com.google.inject.matcher.Matchers;
 import com.google.inject.persist.Transactional;
+import com.oracle.jaxb21.Persistence;
 import za.co.mmagon.guiceinjection.abstractions.GuiceInjectorModule;
 import za.co.mmagon.guiceinjection.annotations.JaxbContext;
+import za.co.mmagon.guiceinjection.exceptions.NoConnectionInfoException;
 import za.co.mmagon.guiceinjection.interfaces.GuiceDefaultBinder;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 
 public class GuicedPersistenceBinding
 		extends GuiceDefaultBinder
@@ -31,7 +35,22 @@ public class GuicedPersistenceBinding
 		module.bindInterceptor(Matchers.any(), Matchers.annotatedWith(Transactional.class), new TransactionHandler());
 		module.bindInterceptor(Matchers.any(), Matchers.annotatedWith(javax.transaction.Transactional.class), new TransactionHandler());
 		module.bind(PERSISTENCE_CONTEXT_KEY)
-		      .toInstance(persistenceContext);
+		      .toProvider(() ->
+		                  {
+			                  if (persistenceContext == null)
+			                  {
+				                  try
+				                  {
+					                  GuicedPersistenceBinding.persistenceContext = JAXBContext.newInstance(Persistence.class);
+				                  }
+				                  catch (JAXBException e)
+				                  {
+					                  throw new NoConnectionInfoException("Persistence Unit Load Failed", e);
+				                  }
+			                  }
+			                  return persistenceContext;
+		                  })
+		      .in(Singleton.class);
 	}
 
 }
