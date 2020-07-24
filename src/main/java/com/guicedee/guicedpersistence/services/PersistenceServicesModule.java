3144a26dacc8e3930d02f5975445722347463f82
GedMarc <ged_marc@hotmail.com>
2020-07-24 03:29:35 +0200

    Updates for Async scanning, and multiple duplicate persistence jndi

--- src/main/java/com/guicedee/guicedpersistence/services/PersistenceServicesModule.java
+++ src/main/java/com/guicedee/guicedpersistence/services/PersistenceServicesModule.java
@@ -17,83 +17,75 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class PersistenceServicesModule
-		extends AbstractModule
-		implements IGuiceModule<PersistenceServicesModule>
-{
-	private static final Logger log = LogFactory.getLog(PersistenceServicesModule.class);
+        extends AbstractModule
+        implements IGuiceModule<PersistenceServicesModule> {
+    private static final Logger log = LogFactory.getLog(PersistenceServicesModule.class);
 
-	private static final Map<Class<? extends Annotation>, Module> modules = new LinkedHashMap<>();
-	private static final Map<Class<? extends Annotation>, ConnectionBaseInfo> jtaConnectionBaseInfo = new LinkedHashMap<>();
+    private static final Map<Class<? extends Annotation>, Module> modules = new LinkedHashMap<>();
+    private static final Map<Class<? extends Annotation>, ConnectionBaseInfo> jtaConnectionBaseInfo = new LinkedHashMap<>();
 
-	private static final Map<String, DataSource> jtaDataSources = new LinkedHashMap<>();
-	private static final Map<String, Set<String>> jtaPersistenceUnits = new LinkedHashMap<>();
+    private static final Map<String, DataSource> jtaDataSources = new LinkedHashMap<>();
+    private static final Map<String, Set<String>> jtaPersistenceUnits = new LinkedHashMap<>();
 
-	@Override
-	protected void configure()
-	{
-		log.config("Building Persistence Services Module");
-		jtaConnectionBaseInfo.forEach((key, value) ->
-		                              {
-			                              try
-			                              {
-				                              if (!jtaDataSources.containsKey(value.getJndiName()))
-				                              {
-					                              log.config("Starting datasource - " + value.getJndiName());
-					                              DataSource ds = value.toPooledDatasource();
-					                              if(ds != null)
-					                              {
-						                              jtaDataSources.put(value.getJndiName(), ds);
-						                              bind(Key.get(DataSource.class,key)).toInstance(ds);
-					                              }
-					                              if(!jtaPersistenceUnits.containsKey(value.getJndiName()))
-					                              {
-						                              jtaPersistenceUnits.put(value.getJndiName(), new LinkedHashSet<>());
-					                              }
-					                              jtaPersistenceUnits.get(value.getJndiName())
-					                                                 .add(jtaConnectionBaseInfo.get(key).getPersistenceUnitName());
-				                              }
-			                              }
-			                              catch (Exception t)
-			                              {
-				                              log.log(Level.SEVERE, "Cannot start datasource!", t);
-			                              }
-		                              });
-		modules.forEach((key, value) -> install(value));
-	}
+    @Override
+    protected void configure() {
+        log.config("Building Persistence Services Module");
+        jtaConnectionBaseInfo.forEach((key, value) ->
+        {
+            boolean datasourceRegistered = false;
+            DataSource ds = null;
+            try {
+                if (!jtaDataSources.containsKey(value.getJndiName())) {
+                    datasourceRegistered = true;
+                    log.config("Starting datasource - " + value.getJndiName());
+                    ds = value.toPooledDatasource();
+                    if (ds != null) {
+                        jtaDataSources.put(value.getJndiName(), ds);
+                        bind(Key.get(DataSource.class, key)).toInstance(ds);
+                    }
+                    if (!jtaPersistenceUnits.containsKey(value.getJndiName())) {
+                        jtaPersistenceUnits.put(value.getJndiName(), new LinkedHashSet<>());
+                    }
+                    jtaPersistenceUnits.get(value.getJndiName())
+                            .add(jtaConnectionBaseInfo.get(key).getPersistenceUnitName());
+                } else {
+                    ds = jtaDataSources.get(value.getJndiName());
+                    if (ds != null)
+                        bind(Key.get(DataSource.class, key)).toInstance(ds);
+                }
+            } catch (Exception t) {
+                log.log(Level.SEVERE, "Cannot start datasource!", t);
+            }
+        });
+        modules.forEach((key, value) -> install(value));
+    }
 
-	public static Map<Class<? extends Annotation>, Module> getModules()
-	{
-		return modules;
-	}
+    public static Map<Class<? extends Annotation>, Module> getModules() {
+        return modules;
+    }
 
-	public static Map<Class<? extends Annotation>, ConnectionBaseInfo> getJtaConnectionBaseInfo()
-	{
-		return jtaConnectionBaseInfo;
-	}
+    public static Map<Class<? extends Annotation>, ConnectionBaseInfo> getJtaConnectionBaseInfo() {
+        return jtaConnectionBaseInfo;
+    }
 
-	public static Map<String, DataSource> getJtaDataSources()
-	{
-		return jtaDataSources;
-	}
+    public static Map<String, DataSource> getJtaDataSources() {
+        return jtaDataSources;
+    }
 
-	public static void addJtaPersistenceUnits(String jndi, String persistenceUnitName)
-	{
-		if (!jtaPersistenceUnits.containsKey(jndi))
-		{
-			jtaPersistenceUnits.put(jndi, new LinkedHashSet<>());
-		}
-		jtaPersistenceUnits.get(jndi)
-		                   .add(persistenceUnitName);
-	}
+    public static void addJtaPersistenceUnits(String jndi, String persistenceUnitName) {
+        if (!jtaPersistenceUnits.containsKey(jndi)) {
+            jtaPersistenceUnits.put(jndi, new LinkedHashSet<>());
+        }
+        jtaPersistenceUnits.get(jndi)
+                .add(persistenceUnitName);
+    }
 
-	public static Map<String, Set<String>> getJtaPersistenceUnits()
-	{
-		return jtaPersistenceUnits;
-	}
+    public static Map<String, Set<String>> getJtaPersistenceUnits() {
+        return jtaPersistenceUnits;
+    }
 
-	@Override
-	public Integer sortOrder()
-	{
-		return Integer.MAX_VALUE - 500;
-	}
+    @Override
+    public Integer sortOrder() {
+        return Integer.MAX_VALUE - 500;
+    }
 }
